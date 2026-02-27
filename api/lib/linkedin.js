5f046c6884348bd54172f4624b426bcf6d37921e
Gaurav <gauravmakhecha@gmail.com>
2026-02-27 11:03:30 +0530

    api version

--- api/lib/linkedin.js
+++ api/lib/linkedin.js
@@ -110,7 +110,7 @@ export async function fetchLinkedInArticles(accessToken) {
   const res = await fetch(url, {
     headers: {
       Authorization: `Bearer ${accessToken}`,
-      'LinkedIn-Version': '202601',
+      'LinkedIn-Version': '202511',
       'X-Restli-Protocol-Version': '2.0.0',
     },
   });
